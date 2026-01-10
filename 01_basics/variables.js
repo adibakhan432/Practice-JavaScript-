91dd90fa384c5914343a695f774229eadbc3413b
Adiba khan <140267295+adibakhan432@users.noreply.github.com>
2026-01-10 11:54:15 +0000

    config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 121212
+let accountEmail = "acb@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "abc1@gmail.com"
+accountPassword = "456987"
+accountCity = "Udaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
