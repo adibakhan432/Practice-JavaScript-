7e7397de8046bbdce44bb79ee16b5d6db7fdc574
Adiba khan <140267295+adibakhan432@users.noreply.github.com>
2026-01-10 12:52:16 +0000

    conversion

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,50 @@
+/*
+Notes:
+
+"33" = 33
+"33abc" = NaN
+true = 1 , false = 0
+
+1 = true, 0 = false
+"" = false
+"xyz" = true
+*/
+
+let score = null
+
+let valueInNumber = Number(score)
+
+//console.log(typeof score);
+//console.log(typeof (score));
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************* Operations ************************
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue);
+
+let str1  = "Hello"
+let str2 = " World"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
