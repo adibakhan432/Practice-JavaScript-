2953d4e129ef01d8e0384b99cfbb98537905e39d
Adiba khan <140267295+adibakhan432@users.noreply.github.com>
2026-01-10 12:09:48 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+/*
+Notes:
+1.Primitive datatypes
+
+number = 2^53 approx range
+bigInt
+string = ""
+boolean = true/false
+null = standalone value
+undefined  = not assign the value
+symbol = unique
+
+2.non-premitive datatypes
+
+object 
+*/
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
