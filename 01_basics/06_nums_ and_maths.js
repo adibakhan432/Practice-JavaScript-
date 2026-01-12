18e5553a5b4af72b7f89941c86a8da0482d760d4
Adiba khan <140267295+adibakhan432@users.noreply.github.com>
2026-01-12 14:20:08 +0000

    nums

---
+++ 01_basics/06_nums_ and_maths.js
@@ -0,0 +1,29 @@
+// Number Properties:-
+
+const balance = new Number(100)
+const cost = 500
+
+//console.log(balance)
+//console.log(cost)
+//console.log(balance.toString())
+//console.log(typeof(balance.toString()))
+
+//console.log(balance.toFixed(1)) // o/p: 100.0
+
+const otherNumber = 123.6666
+
+//console.log(otherNumber.toPrecision(3)) // o/p:124
+
+const num = 1000000
+
+//console.log(num.toLocaleString('en-IN')) // o/p: 1,000,000 - 10,00,000
+
+// ------------------------ Maths ----------------------------------
+
+// Maths Properties:
+
+console.log(Math)
+console.log(Math.abs(-4)) // it gives the output in Positive value
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.3))
+console.log(Math.floor(4.6))
