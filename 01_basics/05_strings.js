f95dd685f2b09fbd15df5b008a36a5aabfd32455
Adiba khan <140267295+adibakhan432@users.noreply.github.com>
2026-01-12 13:28:34 +0000

    Commit Config done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+let firstName = "Nick"
+let lastName = "John"
+
+console.log(`My name is ${firstName} ${lastName}`)
+
+//Declaration of string
+
+const gameName = new String('AdibaKhan432') //create object it gives the output of total string functions.
+//const msg = gameName.anchor('hii')
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.anchor('hello'))
+//console.log(msg)
