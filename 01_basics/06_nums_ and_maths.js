c3e910a9da0f1b48a8d2fb2e327e0dd6777dd365
Adiba khan <140267295+adibakhan432@users.noreply.github.com>
2026-01-13 04:59:45 +0000

    maths and num

--- 01_basics/06_nums_ and_maths.js
+++ 01_basics/06_nums_ and_maths.js
@@ -22,8 +22,17 @@ const num = 1000000
 
 // Maths Properties:
 
-console.log(Math)
-console.log(Math.abs(-4)) // it gives the output in Positive value
-console.log(Math.round(4.6))
-console.log(Math.ceil(4.3))
-console.log(Math.floor(4.6))
+// console.log(Math)
+// console.log(Math.abs(-4)) // it gives the output in Positive value
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.6))
+
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
