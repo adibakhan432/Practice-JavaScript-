4d64504e2cfc9931ba63b16a04ee9cde52c56f1b
Adiba khan <140267295+adibakhan432@users.noreply.github.com>
2026-01-19 09:06:43 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,5 +19,43 @@ function loginUserMessage(username){
     return `${username} just logged in.`
 }
 
-console.log(loginUserMessage("Adiba"));
-console.log(loginUserMessage());  // it gives output is undefined
+//console.log(loginUserMessage("Adiba"));
+//console.log(loginUserMessage());  // it gives output is undefined
+
+function calculateCartPrice(n1, n2, ...val1){
+    return val1
+
+}
+
+//console.log(calculateCartPrice(200, 100, 300, 400));
+
+//Function with Objects
+
+const user = {
+    username : "Ram",
+    price : 100
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and Price is ${anyobject.price}`);
+}
+
+//handleObject(user)
+
+handleObject({
+    username : "sam",
+    price :  100
+})
+
+//Function with Array
+
+const arr = [100, 200, 300, 400, 500]
+
+function handleArray(anyarray){
+    //console.log(`This is my Array ${anyarray[1]}`)
+    return anyarray[0]
+}
+
+console.log(handleArray([
+    500,200
+]))
