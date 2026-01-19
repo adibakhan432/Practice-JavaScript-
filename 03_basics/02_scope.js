9e79cc04d59ece7d98346c233147a48f9d52f871
Adiba khan <140267295+adibakhan432@users.noreply.github.com>
2026-01-19 09:35:13 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,38 @@
+function one(){
+    const username = "Sam"
+
+    function two(){
+        const website = "YouTube"
+        //console.log(username)
+    }
+    //console.log(website)
+
+    two()
+}
+
+one()
+
+if(true){
+    const name = "Som"
+
+    if(name === "Som"){
+        const web = " devops"
+        //console.log(name + web)
+    }
+
+    //console.log(web);
+}
+//console.log(name)
+
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1 
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2 
+}
+
